8129053ddb0f02042e2c8b6a0842d060441de7c9
Ali Masri <alimasri1991@gmail.com>
2023-06-05 23:20:42 -0400

    rewrite the upload-to-gcs function

--- src/test.py
+++ src/test.py
@@ -1,7 +1,20 @@
 from google.cloud import storage
-storage_client = storage.Client()
-bucket=storage_client.get_bucket("daily-game-stats")
-blob = bucket.blob('my-blob')
+from pathlib import Path
 
-with open('out/game_info.csv', 'rb') as my_file:
-    blob.upload_from_file(my_file)
+def upload_to_cloud_storage(bucket_name: str, file_path: Path) -> None:
+    if not file_path.exists():
+        raise FileNotFoundError(f"File {file_path} does not exist")
+    if not file_path.is_file():
+        raise FileNotFoundError(f"{file_path} is not a file")
+    
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    if not bucket.exists():
+        bucket.create()
+    
+    blob = bucket.blob(file_path.name)
+    blob.upload_from_filename(file_path)
+
+
+if __name__ == "__main__":
+    upload_to_cloud_storage("daily-game-stats", Path("out/game_info.csv"))
---
+++ src/upload_to_cloud_storage.py
@@ -0,0 +1,7 @@
+from google.cloud import storage
+storage_client = storage.Client()
+bucket=storage_client.get_bucket("daily-game-stats")
+blob = bucket.blob('game_info.csv')
+
+with open('out/game_info.csv', 'rb') as my_file:
+    blob.upload_from_file(my_file)
