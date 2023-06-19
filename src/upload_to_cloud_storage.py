3163f046a2b33d21611b51460947588da5f4cc55
kliang696 <kliang696@gwu.edu>
2023-06-19 17:20:40 -0400

    upload to big query

--- src/upload_to_cloud_storage.py
+++ src/upload_to_cloud_storage.py
@@ -1,7 +1,24 @@
-from google.cloud import storage
-storage_client = storage.Client()
-bucket=storage_client.get_bucket("daily-game-stats")
-blob = bucket.blob('game_info.csv')
+from google.cloud import bigquery
+# Instantiate a BigQuery client
+bq_client = bigquery.Client()
 
-with open('out/game_info.csv', 'rb') as my_file:
-    blob.upload_from_file(my_file)
+source_uri = "gs://daily-game-stats/game_info.csv"
+dataset_id = "my_dataset"
+table_id = "my_table"
+
+# Define the job configuration for the load job
+job_config = bigquery.LoadJobConfig(
+    autodetect=True,  # Auto-detect the schema
+    source_format=bigquery.SourceFormat.CSV,  # Define source format
+)
+
+# Define the destination table reference
+table_ref = bq_client.dataset(dataset_id).table(table_id)
+
+# Start the load job
+load_job = bq_client.load_table_from_uri(source_uri, table_ref, job_config=job_config)
+
+# Wait for the job to complete
+load_job.result()
+
+print("Data loaded successfully.")
